efaeee06db6112e6c1e004f09b486df220946e48
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-06-25 12:51:56 -0400

    Create grid.java

---
+++ grid.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+
+class TileGen {
+    //this class will make a tile object for every tile and store it in a vector
+}
+//this class will in the end pass an array of arrays that will represent the grid
+//@attributes
+// - it will have an int for the amount of mines around it, empty if none
+// - it will have a boolean for whether or not the tile IS a mine
+//
+//i plan on storing each individual tile object in a Vector stored inside of tile gen, then i will create an array
+//with all the tile objects inside it !!(there is a chance that you cannot do what i want) may have to stick with Vectors!!
+class Tile {
+    private Vector<String> TEMP = new Vector<> ();
+
+}
