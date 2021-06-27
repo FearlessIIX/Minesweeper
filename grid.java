1bcc7f961c1fcd34d0e44e70ffd45dcdd0444825
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-06-27 19:29:14 -0400

    Update grid.java

--- grid.java
+++ grid.java
@@ -1,16 +1,43 @@
-import java.util.Vector;
+import java.util.*;
 
 class TileGen {
-    //this class will make a tile object for every tile and store it in a vector
-}
-//this class will in the end pass an array of arrays that will represent the grid
-//@attributes
-// - it will have an int for the amount of mines around it, empty if none
-// - it will have a boolean for whether or not the tile IS a mine
-//
-//i plan on storing each individual tile object in a Vector stored inside of tile gen, then i will create an array
-//with all the tile objects inside it !!(there is a chance that you cannot do what i want) may have to stick with Vectors!!
-class Tile {
-    private Vector<String> TEMP = new Vector<> ();
+    private int gdx;
+    private int gdy;
+    private Vector<Tile> RET;
 
+    public TileGen(Vector MINES, int grid_x, int grid_y) {
+        this.gdx = grid_x;
+        this.gdy = grid_y;
+        Vector<Tile> GAME_GRID = new Vector<>();
+        //creates each tile object, then adds it to the vector
+        for (int t_am = 1; t_am < grid_x * grid_y + 1; t_am++) {
+            boolean m_tag = false;
+            for (Object mine : MINES) {
+                if (Integer.toString(t_am).equals(mine)) m_tag = true;
+            }
+            Tile tile_made = new Tile(m_tag, t_am);
+            GAME_GRID.addElement(tile_made);
+        }
+        //System.out.println(GAME_GRID);
+        appendTileSur(GAME_GRID);
+
+        this.RET = GAME_GRID;
+    }
+
+    private void appendTileSur(Vector<Tile> GG) {
+        for (int countZ = 0; countZ < GG.size(); countZ++) {
+            GG.get(countZ).genSur(GG, this.gdx, this.gdy);
+
+
+        }
+    }
+    public void pr(Vector<Tile> vec, int start, int xc) {
+        for (int x = start; x < xc; x++) {
+            System.out.print(vec.get(x) + " ");
+        }
+        System.out.print("\n");
+    }
+    public Vector getRet() {
+        return this.RET;
+    }
 }
