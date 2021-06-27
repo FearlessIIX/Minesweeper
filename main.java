ef9c70a0e41e21a22b44bbd11e9d0cd60cb0cc03
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-06-27 19:29:47 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,8 +1,21 @@
 import java.util.*;
-class Main {
+import javax.swing.*;
+public class Main {
+
+    public static void clear() {
+        System.out.println(new String(new char[50]).replace("\0", "\r\n"));
+    }
+
     public static void main(String[] args) {
 
+        // this is manually changed. Options are: 'index' , and 'cord'
+        String method = "cord";
+
         Begin SETUP = new Begin();
+
+        System.out.println("\n \n \n \n \n \n \n \n \n \n ");
+        //clear();  //clears the console after startup
+
         int mines = SETUP.fetch("mines");
         int width = SETUP.fetch("width");
         int height = SETUP.fetch("height");
@@ -10,7 +23,14 @@ class Main {
         Generate GEN = new Generate(width, height, mines);
         Vector<String> MINES = GEN.getMines();
 
+        TileGen GEN_CON = new TileGen(MINES, width, height);
+        Vector<Tile> STORE = GEN_CON.getRet();
 
+        Game GAME = new Game(STORE, width, height);
+        GAME.changeMethod(method);
+        Screen SCREEN = new Screen();
 
+        Play PLAY = new Play(GAME, SCREEN);
+        PLAY.execute();
     }
 }
