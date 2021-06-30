378bb9d0d1544724531a0c3576447fe4cc910e69
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-06-30 15:56:04 -0400

    Finished play.java

--- play.java
+++ play.java
@@ -20,70 +20,101 @@ class Play {
 
         //TODO: put Java sleep Method here
 
-        //Main.clear();
-        boolean done = false;
         String stage = "D_GRID";
-        while (!(done)) {
-            if (stage.equals("D_GRID")) {
-                //code for displaying the grid to the screen
-                //grid will also include a prompt for the user to input Tile
-                this.SCREEN.display(this.GAME);
-                if (this.lost) {
-                    System.out.println("\n \n You hit a mine!!");
-                    break;
+        outer:
+        while (true) {
+            switch (stage) {
+                case "D_GRID" -> {
+                    //code for displaying the grid to the screen
+                    //grid will also include a prompt for the user to input Tile
+                    this.SCREEN.display(this.GAME);
+                    if (this.lost) {
+                        System.out.println("\n \n You hit a mine!!");
+                        break outer;
+                    }
+                    stage = "G_INPUT";
                 }
-                stage = "G_INPUT";
+                case "G_INPUT" -> {
+                    //code for getting the user input
 
-            }
-            else if (stage.equals("G_INPUT")) {
-                //code for getting the user input
-                //char[] alpha = "abcdefghijklmnopqrstuvwxyz`~!@#$%^&*()_+[{]}\\|;:'\",<.>/?".toCharArray();
-
-                String usr_input = SCREEN.userInput();
-                //no way to validate user input for now
-                if (GAME.getInputMethod() == "index") {
-                    this.input = Integer.parseInt(usr_input);
-                }
-                else {
-                    this.first_in = Integer.parseInt(usr_input);
-                    usr_input = SCREEN.userInput();
-                    this.second_in = Integer.parseInt(usr_input);
-                }
-                Main.clear();
-                stage = "DO_EXEC";
-            }
-            else if (stage.equals("DO_EXEC")) {
-                //code for handling the revealing of Tiles
-                //this will also handle wining and losing the game
-
-                Vector<Tile> grid = GAME.getGrid();
-                if (GAME.getInputMethod().equals("index")) {
-
+                    String usr_input = SCREEN.userInput();
+                    //no way to validate user input for now
                     try {
-                        Tile item = grid.get(this.input - 1);
-                        if (item.isMine()) this.lost = true;
-                        else item.reveal();
+                        if (GAME.getInputMethod().equals("index")) {
+                            try {
+                                this.input = Integer.parseInt(usr_input);
+                            }
+                            catch (Exception e) {
+
+                            }
+
+                        } else {
+                            this.first_in = Integer.parseInt(usr_input);
+                            usr_input = SCREEN.userInput();
+                            this.second_in = Integer.parseInt(usr_input);
+                        }
                     } catch (Exception e) {
                         //skips this iteration
                     }
+                    Main.clear();
+                    stage = "DO_EXEC";
                 }
-                else {
+                case "DO_EXEC" -> {
+                    //code for handling the revealing of Tiles
+                    //this will also handle wining and losing the game
 
-                    try {
-                        int first = this.first_in - 1;
-                        int second = this.second_in;
+                    Vector<Tile> grid = GAME.getGrid();
+                    if (GAME.getInputMethod().equals("index")) {
 
-                        int tile_index = (first * GAME.getWidth()) + second;
+                        try {
+                            Tile item = grid.get(this.input - 1);
+                            if (item.isMine()) this.lost = true;
+                            else {
+                                item.reveal();
+                                if (item.getSafety() == 0) {
+                                    //block to be ran when zeros are encountered
+                                    Reveal rev = new Reveal(GAME.getGrid());
+                                    rev.zeroGrid(this.input - 1, GAME.getWidth());
+                                }
+                            }
+                        } catch (Exception e) {
+                            //skips this iteration
+                        }
+                    } else {
 
-                        Tile item = grid.get(tile_index - 1);
-                        if (item.isMine()) this.lost = true;
-                        item.reveal();
+                        try {
+                            int first = this.first_in - 1;
+                            int second = this.second_in;
+
+                            int tile_index = (first * GAME.getWidth()) + second;
+
+                            Tile item = grid.get(tile_index - 1);
+                            if (item.isMine()) this.lost = true;
+
+                            if (item.getSafety() == 0) {
+                                //block to be ran when zeros are encountered
+                                if ((!item.isRevealed())) {
+                                    Reveal rev = new Reveal(GAME.getGrid());
+                                    rev.zeroGrid(tile_index - 1, GAME.getWidth());
+                                }
+                            }
+                            item.reveal();
+                        } catch (Exception e) {
+                            //skips this iteration
+                        }
                     }
-                    catch (Exception e) {
-                        //skips this iteration
+                    int count_revealed = 0;
+                    for (Tile tile : GAME.getGrid()) {
+                        if (!(tile.isMine())) {
+                            if (tile.isRevealed()) count_revealed++;
+                        }
+                        if (count_revealed + GAME.getMines() == GAME.getGrid().size()) {
+                            System.out.println("You Win!!!!");
+                            break outer;
+                        }
                     }
+                    stage = "D_GRID";
                 }
-                stage = "D_GRID";
             }
         }
     }
